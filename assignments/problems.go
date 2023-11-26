5f75f324f687ea5fc38e5fb888985b95c759839a
TecLegacy <kshvkumar.kk2@gmail.com>
2023-11-26 19:21:58 +0530

    badWord using range keyword

--- assignments/problems.go
+++ assignments/problems.go
@@ -3,7 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fizzBud(100)
+	testBadWord()
+}
+
+//* Index of first badword
+func testBadWord() {
+	badWord := []string{
+		"stupid",
+		"idiot",
+		"duffer",
+	}
+	message := []string{"I", "Love", "you", "stupid"}
+
+	index := indexOfFirstBadWord(message, badWord)
+
+	fmt.Printf("The index of badword %d", index+1)
+}
+
+func indexOfFirstBadWord(msg, badWords []string) int {
+	// On^2 time complexity
+	for i, word := range msg {
+		for _, badWord := range badWords {
+			if badWord == word {
+				return i
+			}
+		}
+	}
+	return -1
 }
 
 //* fizzbuzz classic problem
